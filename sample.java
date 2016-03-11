30f31d0fd8584579da411d7524943de556037e8c
yanli2015 <yan.li1@udc.edu>
2016-03-10 19:14:22 -0500

    //

--- sample.java
+++ sample.java
@@ -1,6 +1,6 @@
 package com.example.tests;
 
-import java.util.regex.Pattern;
+//import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
 import org.junit.*;
 import static org.junit.Assert.*;
